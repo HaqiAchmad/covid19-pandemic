dfef4bf6f60ec9a9478e61afc681ef7e5d793e44
Achmad Baihaqi <hakiahmad756@gmail.com>
2020-11-19 20:12:27 +0700

    update covid casses

--- src/com/database/CovidCases.java
+++ src/com/database/CovidCases.java
@@ -1,5 +1,21 @@
 package com.database;
 
-public class CovidCases {
+public class CovidCases extends Database{
+
+    private final String TABEL;
+    public static final String CASE_WORLD = "kasuscovid_dunia",
+                               CASE_INDO = "kasuscovid_indo";
+    
+    public CovidCases(final String tabel){
+        TABEL = tabel;
+        this.startConnection();
+    }
+    
+    public static void main(String[] args) {
+        CovidCases covid = new CovidCases(CASE_INDO);
+        
+        System.out.println(covid.TABEL);
+        covid.closeConnection();
+    }
     
 }
