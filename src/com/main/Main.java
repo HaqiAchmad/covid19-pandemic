d055d22a00b46e2740f9228db8d28add761154fe
Achmad Baihaqi <hakiahmad756@gmail.com>
2020-11-20 19:24:06 +0700

    update main.java

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,6 +3,14 @@ package com.main;
 public class Main {
     
     public static void main(String[] args) {
-
+        
+        java.awt.EventQueue.invokeLater(new Runnable(){
+            
+            @Override
+            public void run(){
+                new com.window.all.LoadingWindow().setVisible(true);
+            }
+        });
+        
     }
 }
